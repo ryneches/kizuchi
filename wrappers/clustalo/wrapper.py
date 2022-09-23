c52213c95d185c025c363d45df577fc367f02f06
Russell Neches <russell@vort.org>
2022-09-23 19:41:19 +0900

    test for empty alignment input

--- wrappers/clustalo/wrapper.py
+++ wrappers/clustalo/wrapper.py
@@ -13,11 +13,22 @@ extra = snakemake.params.get("extra", "")
 # Formats the log redrection string
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-# Executed shell command
-shell(
-    "clustalo {extra}"
-    " --threads={snakemake.threads}"
-    " --in {snakemake.input[0]}"
-    " --out {snakemake.output[0]} "
-    " {log}"
-)
+# if there are fewer than two records in the input file, write an
+# empty output file and exit.
+if open( snakemake.input[0] ).read().count( '>' ) < 3 :
+    
+   # with open( snakemake.log, 'w' ) as f :
+   #     f.write( 'Too few records to perform an alignment.' )
+    
+    with open( snakemake.output[0], 'w' ) as f :
+        f.write( '' )
+else :
+
+    # Executed shell command
+    shell(
+        "clustalo {extra}"
+        " --threads={snakemake.threads}"
+        " --in {snakemake.input[0]}"
+        " --out {snakemake.output[0]} "
+        " {log}"
+    )
