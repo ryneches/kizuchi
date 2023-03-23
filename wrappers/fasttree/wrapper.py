8e8da3d3b5b3283729040c8325ab6d229fa95423
Russell Neches <russell@vort.org>
2023-03-23 16:40:59 +0900

    improve logging

--- wrappers/fasttree/wrapper.py
+++ wrappers/fasttree/wrapper.py
@@ -7,25 +7,24 @@ __license__ = "MIT"
 from snakemake.shell import shell
 import os
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
-extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell( stdout=True, stderr=True )
+extra = snakemake.params.get( 'extra', '' )
 
 # if there are fewer than two records in the input file, write an
 # empty output file and exit.
 if open( snakemake.input[0] ).read().count( '>' ) < 3 :
     
-   # with open( snakemake.log, 'w' ) as f :
-   #     f.write( 'Too few records to perform an alignment.' )
-    
-    with open( snakemake.output[0], 'w' ) as f :
-        f.write( '' )
+   with open( snakemake.log, 'w' ) as f :
+      f.write( 'Too few records to perform an alignment.' )
+   with open( snakemake.output[0], 'w' ) as f :
+      f.write( '' )
 
 else :
 
     shell(
-        "fasttree "
-        "{extra} "
-        "{snakemake.input.alignment} "
-        "> {snakemake.output.tree} "
-        "{log}"
+        'fasttree'
+        ' {extra}'
+        ' -out {snakemake.output.tree}'
+        ' -log {snakemake.log[0]}'
+        ' {snakemake.input.alignment}'
     )
--- wrappers/trimal/wrapper.py
+++ wrappers/trimal/wrapper.py
@@ -7,24 +7,24 @@ __license__ = 'MIT'
 from snakemake.shell import shell
 import os
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+log = snakemake.log_fmt_shell( stdout=True, stderr=True )
 extra = snakemake.params.get( 'extra', '' )
 
 # if there are fewer than two records in the input file, write an
 # empty output file and exit.
 if open( snakemake.input[0] ).read().count( '>' ) < 3 :
     
-   # with open( snakemake.log, 'w' ) as f :
-   #     f.write( 'Too few records to perform an alignment.' )
-    
-    with open( snakemake.output[0], 'w' ) as f :
-        f.write( '' )
+   with open( snakemake.log[0], 'w' ) as f :
+      f.write( 'Too few records to perform an alignment.' )
+   with open( snakemake.output[0], 'w' ) as f :
+      f.write( '' )
 
 else :
 
     shell(
         'trimal '
-        '-in {snakemake.input} '
-        '-out {snakemake.output} '
-        '{extra}'
+        ' -in {snakemake.input}'
+        ' -out {snakemake.output}'
+        ' {extra}'
+        ' {log}'
     )
